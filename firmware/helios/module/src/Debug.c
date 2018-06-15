275c30a97210644b59713e39fdeaac15f88c9658
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2018-06-15 15:50:54 +0200

    Debugs the debug module

--- firmware/helios/module/src/Debug.c
+++ firmware/helios/module/src/Debug.c
@@ -9,18 +9,26 @@ typedef struct {
 	uint8_t value;
 	uint8_t error;
 	uint8_t displayed;
+	uint16_t lastDisplayed;
 } DebugData_t;
 
 DebugData_t debug;
 
+void Ddisplay(uint8_t data) {
+	uint8_t toDisplay = data << 4;
+	PORTA &= (~0x30) | toDisplay;
+	PORTA |= 0x30 & toDisplay;
+}
+
 //Bit 0 : A5
 //Bit 1 : A4
-
 void InitDebug() {
 	DDRA |= _BV(5) | _BV(4);
+	Ddisplay(0);
 	debug.value = 0;
 	debug.error = 0;
 	debug.displayed = 0;
+	debug.lastDisplayed = 0;
 }
 
 void Log(uint8_t data) {
@@ -32,29 +40,32 @@ void ReportError(uint8_t error) {
 }
 
 void DProcess() {
-	if ( (LMSystime() & 0x7f) != 0 ) {
+	uint16_t now = LMSystime();
+	if ( ( now - debug.lastDisplayed ) < 305 ) {
 		return;
 	}
-	debug.displayed = (debug.displayed + 1) & 0x07;
+	debug.lastDisplayed = now;
+
+	++debug.displayed;
 	if (debug.error == 0) {
 		if (debug.displayed >= 4) {
-			PORTA &= ~(_BV(5) | _BV(4) ) ;
+			Ddisplay(0);
+			debug.displayed = 0xff;
 			return;
 		}
-		uint8_t toPrint = ((debug.value >> debug.displayed) & 0x03) << 4;
-		PORTA &= toPrint || ~0x30;
-		PORTA |= toPrint;
+		Ddisplay(debug.value >> (2*debug.displayed));
 		return;
 	}
 
-if ( (debug.displayed & 0x01) == 1) {
-		PORTA &= ~(_BV(5) | _BV(4) ) ;
+	if ( (debug.displayed & 0x01) == 1) {
+		Ddisplay(0);
+
 		return;
 	}
-
-	uint8_t toPrint = ((debug.value >> (debug.displayed >> 1) ) & 0x03) << 4;
-	PORTA &= toPrint || ~0x30;
-	PORTA |= toPrint;
-
+	if(debug.displayed < 8) {
+		Ddisplay(debug.error >> debug.displayed);
+	} else {
+		debug.displayed = 0xff;
+	}
 
 }
--- firmware/helios/module/src/main.c
+++ firmware/helios/module/src/main.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 
-//#include "Debug.h"
+#include "Debug.h"
 #include "LightManager.h"
 //#include "ChargeMonitor.h"
 #include <avr/io.h>
@@ -8,34 +8,17 @@
 #include <inttypes.h>
 #include <util/delay.h>
 
-void display(uint8_t data) {
-	uint8_t toDisplay = data << 4;
-	PORTA &= (~0x30) | toDisplay;
-	PORTA |= 0x30 & toDisplay;
-}
-
-
-
 int main() {
 
 
-	//	InitDebug();
+	InitDebug();
 	//	InitChargeMonitor();
 	InitLightManager();
 	//	Log(0);
-	DDRA |= _BV(5) | _BV(4);
 
-	uint8_t seconds = 1;
-	display(seconds);
-	uint16_t start = LMSystime();
+	ReportError(0xff);
 	while(true) {
-
-		uint16_t now = LMSystime();
-		if ( now - start >= 610 ) {
-			display(++seconds);
-			start = now;
-		}
-		//DProcess();
+		DProcess();
 	}
 
 	return 0;
