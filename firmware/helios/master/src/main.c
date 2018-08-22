dc5a9c6f023fb46bc9e97fc4e56ad24ffb98ae29
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2018-08-22 13:05:28 +0200

    Makes the master a very basic clock

--- firmware/helios/master/src/main.c
+++ firmware/helios/master/src/main.c
@@ -1,4 +1,19 @@
+#include <avr/io.h>
+
+#include <util/delay.h>
 
 int main() {
+	DDRD |= _BV(2) | _BV(4);
+
+
+	do {
+		_delay_ms(500);
+		PORTD |= _BV(2) | _BV(4);
+		_delay_ms(500);
+		PORTD &= ~(_BV(2) | _BV(4));
+
+	}while(1);
+
+
 	return 0;
 }
