24819271819cec8be568d2edb21f6aa03c6e812b
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2018-10-11 11:44:39 +0200

    [yaacl] Simplifies yaacl_idt memory model
    SImpler to share memory with other higher level libraries

--- firmware/lib/yaacl/src/idt.h
+++ firmware/lib/yaacl/src/idt.h
@@ -3,58 +3,24 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
-struct s_yaacl_idt_std_t {
-	bool rb0:1;
-	bool reserved0:1;
-	bool rtr:1;
-	uint32_t reserved1:18;
-	uint16_t ID:11;
-} __attribute__((packed)) ;
-typedef struct s_yaacl_idt_std_t yaacl_idt_std_t;
+#define YAACL_STD_ID_MSK (((1L << 11) - 1) & 0xffffffff)
+#define YAACL_EXT_ID_MSK (((1L << 29) - 1) & 0xffffffff)
+#define YAACL_IDEBIT_MSK ((1L << 29) & 0xffffffff)
+#define YAACL_RTRBIT_MSK ((1L << 30) & 0xffffffff)
 
-struct s_yaacl_idt_ext_t {
-	uint8_t rb:2;
-	bool rtr:1;
-	uint32_t ID:29;
-} __attribute__((packed));
-typedef struct s_yaacl_idt_ext_t yaacl_idt_ext_t;
+typedef uint32_t yaacl_idt_t;
 
-typedef union {
-	uint32_t as_int_32;
-	struct s_yaacl_idt_std_t std;
-	struct s_yaacl_idt_ext_t ext;
-	uint8_t data[4];
-} yaacl_idt_t;
+#define yaacl_idt_is_ext(idt) ( ((idt) & YAACL_IDEBIT_MSK) != 0 )
 
 
 
-#define yaacl_idt_is_ext(idt) ( (idt).std.rb0 == 1 )
-
-#define yaacl_make_std_idt(idt,id,rtrbit) do{	\
-		(idt).std.rb0 = 0; \
-		(idt).std.rtr = ( (rtrbit) != 0); \
-		(idt).std.reserved0 = 0; \
-		(idt).std.reserved1 = 0; \
-		(idt).std.ID = (id); \
+#define yaacl_make_std_idt(idt,id,rtrbit) do{	  \
+		(idt) = ((id) & YAACL_STD_IDT_MSK) \
+			| ( (rtrbit != 0 ) ? YAACL_RTRBIT_MSK : 0 ); \
 	}while(0)
 
-#define yaacl_make_ext_idt(idt,id,rtrbit) do{	\
-		(idt).ext.rb = 3; \
-		(idt).ext.rtr = ( (rtrbit) != 0); \
-		(idt).ext.ID = (id); \
-	}while(0)
-
-
-#define yaacl_make_std_mask(msk,id,idebit,rtrbit) do{	  \
-		(msk).std.rb0 = idebit; \
-		(msk).std.rtr = ( (rtrbit) != 0); \
-		(msk).std.reserved0 = 0; \
-		(msk).std.reserved1 = 0; \
-		(msk).std.ID = (id); \
-	}while(0)
-
-#define yaacl_make_ext_mask(msk,id,idebit,rtrbit) do{	  \
-		(msk).ext.rb = (idebit != 0 ? 0x1 : 0x0); \
-		(msk).ext.rtr = ( (rtrbit) != 0); \
-		(msk).ext.ID = (id); \
+#define yaacl_make_ext_idt(idt,id,rtrbit) do{	  \
+		(idt) = ((id) & YAACL_EXT_IDT_MSK) \
+			| ( (rtrbit != 0 ) ? YAACL_RTRBIT_MSK : 0 ) \
+			| YAACL_IDEBIT_MSK; \
 	}while(0)
