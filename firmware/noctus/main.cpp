e16a391abd3840359dc86eb945a56bb7ec4c0f4e
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-10-28 17:52:35 +0100

    Adds skeleton for a basic firmware

---
+++ firmware/noctus/main.cpp
@@ -0,0 +1,22 @@
+#include "pico/stdio.h"
+#include "pico/time.h"
+#include <cstdio>
+
+int main() {
+	stdio_init_all();
+
+	auto last = delayed_by_ms(get_absolute_time(), -1000);
+
+	int i = 0;
+	while(true) {
+		auto now = get_absolute_time();
+
+		if ( absolute_time_diff_us(last, now) < 1000000 ) {
+			continue;
+		}
+
+		last = delayed_by_ms(last, 1000);
+		printf("Coucou %d\n",++i);
+
+	}
+}
