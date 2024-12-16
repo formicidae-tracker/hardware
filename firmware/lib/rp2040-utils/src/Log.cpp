b54df32f7937eca35ba51244a72de5889be3b021
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-12-16 21:44:25 +0100

    Imports Logger from IVP's pellet-dispenser

---
+++ firmware/lib/rp2040-utils/src/Log.cpp
@@ -0,0 +1,51 @@
+#include "Log.hpp"
+#include <cstdio>
+#include <optional>
+#include <stdio.h>
+
+void Logger::Logf(Level level, const char *fmt, va_list args) {
+	if (level > d_level) {
+		return;
+	}
+
+	auto now = get_absolute_time();
+
+	auto written =
+	    vsnprintf(d_buffer.data() + d_start, BufferSize - d_start, fmt, args);
+
+	while (d_start + written >= BufferSize) {
+		if (d_start == 0) {
+			d_buffer[BufferSize - 1] = 0;
+
+			written = BufferSize - 1;
+		} else {
+
+			d_start = 0;
+			written = vsnprintf(d_buffer.data(), BufferSize, fmt, args);
+		}
+	}
+	va_end(args);
+
+	Message m = {
+	    .Value = d_buffer.data() + d_start,
+	    .Time  = now,
+	    .Level = level,
+	};
+
+	if (d_queue.TryAdd(std::move(m)) == true) {
+		d_start += written + 1;
+	}
+}
+
+std::optional<Logger::Message> Logger::Pop() {
+	struct Message msg;
+	if (d_queue.TryRemove(msg) == false) {
+		return std::nullopt;
+	}
+
+	return msg;
+}
+
+Logger::Logger() {
+	d_buffer[BufferSize] = 0;
+}
