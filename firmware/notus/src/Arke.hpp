224f3418145753e10bb38dd9ce41ce936ba01f9d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-12-16 18:06:10 +0100

    Basis for an ArkeModule ( to be shared ??)

---
+++ firmware/notus/src/Arke.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstddef>
+#include <optional>
+
+#include <pico/time.h>
+
+void ArkeInit();
--- firmware/notus/src/main.cpp
+++ firmware/notus/src/main.cpp
@@ -1,3 +1,4 @@
+#include "Arke.hpp"
 #include "hardware/irq.h"
 #include "pico/stdio.h"
 #include "pico/time.h"
@@ -31,14 +32,7 @@ std::optional<int64_t> print_uptime(absolute_time_t) {
 int main() {
 	stdio_init_all();
 
-	can2040_setup(&cbus, 0);
-	can2040_callback_config(&cbus, can2040_cb);
-
-	irq_set_exclusive_handler(PIO0_IRQ_0, PIOx_IRQHandler);
-	irq_set_priority(PIO0_IRQ_0, 1);
-	irq_set_enabled(PIO0_IRQ_0, true);
-
-	can2040_start(&cbus, SYS_CLK_HZ, 250000, 13, 12);
+	ArkeInit();
 
 	Scheduler::Schedule(100, 1000000, &print_uptime);
 	while (true) {
