653008b6f75433e2b7702dc6b3c0e84def7bc1de
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-12-16 23:21:27 +0100

    sketches API for rp2040-arke

--- firmware/lib/rp2040-arke/src/Arke.cpp
+++ firmware/lib/rp2040-arke/src/Arke.cpp
@@ -1,1 +1,12 @@
 #include "Arke.hpp"
+
+#include <Log.hpp>
+
+void ArkeInit(ArkeConfig &&config) {
+
+	Infof(
+	    "Arke initialized with class 0x%x and ID=%d",
+	    config.Class,
+	    config.ID
+	);
+}
--- firmware/lib/rp2040-arke/src/Arke.hpp
+++ firmware/lib/rp2040-arke/src/Arke.hpp
@@ -1,5 +1,25 @@
 #pragma once
 
+#include <arke.h>
 
+#include <cstddef>
+#include <functional>
 
-void ArkeInit();
+struct ArkeEvent {
+	ArkeMessageClass Class;
+	bool             RTR = false;
+	uint8_t          Size;
+	const uint8_t   *Data;
+};
+
+struct ArkeConfig {
+
+	int           CanRX, CanTX;
+	ArkeNodeClass Class;
+	ArkeNodeClass ClassMask;
+	uint8_t       ID = 1;
+
+	std::function<void(ArkeEvent)> Callback;
+};
+
+void ArkeInit(ArkeConfig &&config);
