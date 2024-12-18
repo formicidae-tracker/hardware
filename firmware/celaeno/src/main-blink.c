3be1ff2ca70e3c9fc226582053244049b3edef77
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-12-18 13:42:09 +0100

    Corrects Celaeno Fuses
    Moving to PORTF enforces us to disable JTAG, otherwise we cannot use this port.

--- firmware/celaeno/src/main-blink.c
+++ firmware/celaeno/src/main-blink.c
@@ -1,4 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
 int main() {
+
+	DDRF |= _BV(5) | _BV(6);
+	DDRB |= _BV(6);
+	PORTB |= _BV(6);
+	PORTF |= _BV(5) | _BV(6);
+
+	int i = 0;
+	while(1) {
+		++i;
+		_delay_ms(1000);
+		PORTF = (i &0x3) << 5;
+	}
 
 	return 0;
 }
