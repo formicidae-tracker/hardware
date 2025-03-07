9588aeb52898afa0a419c47a43184c0c9a7a0296
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-03-07 13:49:24 +0100

    [Nyx/Firmware] Sets the clock to 10MHz on startup.

--- firmware/nyx/src/main.cpp
+++ firmware/nyx/src/main.cpp
@@ -4,19 +4,31 @@
 #define PIN      1
 #define DELAY_MS 500
 
-int main() {
-	init_system_clock();
-	PORTA.DIRSET = _BV(1);
-	auto last    = get_absolute_time();
+inline void init() {
+	// enable clock change
+	CCP               = CCP_IOREG_gc;
+	// Enable a 10MHz clock for a 3.3V power supply.
+	CLKCTRL.MCLKCTRLB = CLKCTRL_PDIV_2X_gc | CLKCTRL_PEN_bm;
+	CCP               = 0; // Protect the clock again.
 
-	constexpr auto period_tck = MSToTicks(500U); // 500ms
+	init_system_clock();
+
+	PORTA.DIRSET = _BV(1);
+}
+
+int main() {
+	init();
+
+	auto last = get_absolute_time();
+
+	constexpr auto period_tck = MSToTicks(10U); // 500ms
 
 	for (;;) {
 		auto now = get_absolute_time();
 		if ((now - last) < period_tck) {
 			continue;
 		}
-		last = now;
+		last += period_tck;
 		PORTA.OUT ^= _BV(1);
 	}
 	return 0;
